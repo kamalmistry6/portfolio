6efda37d30d1ef214f3f643e341100e793d03ae0
kamalmistry6 <.>
2026-02-06 09:58:52 +0530

    new code

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+/**
+ * Preloader
+ */
+const preloader = document.querySelector("#preloader");
+if (preloader) {
+  window.addEventListener("load", () => {
+    preloader.remove();
+  });
+}
+
+/**
+ * Scroll top button
+ */
+let scrollTop = document.querySelector(".scroll-top");
+
+function toggleScrollTop() {
+  if (scrollTop) {
+    window.scrollY > 100
+      ? scrollTop.classList.add("active")
+      : scrollTop.classList.remove("active");
+  }
+}
+scrollTop.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("load", toggleScrollTop);
+document.addEventListener("scroll", toggleScrollTop);
+
+/**
+ * Init typed.js
+ */
+const selectTyped = document.querySelector(".typed");
+if (selectTyped) {
+  let typed_strings = selectTyped.getAttribute("data-typed-items");
+  typed_strings = typed_strings.split(",");
+  new Typed(".typed", {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+  });
+}
